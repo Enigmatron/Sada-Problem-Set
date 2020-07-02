c4241303a2ab362bf64c15973ca8de080dc7d1e0
Idries Kysia <idries-kysia@uiowa.edu>
2020-07-02 17:40:14 -0500

    Revert "answer3 - 2"
    This reverts commit 42e631be770a911f386a042f82613c6cb38f2153.

--- Main.java
+++ Main.java
@@ -3,20 +3,13 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-//import org.json.simple.JSONArray; 
-//import org.json.simple.JSONObject;
-
-import sun.misc.Queue; 
+import org.json.simple.JSONArray; 
+import org.json.simple.JSONObject; 
 
 public class Main {
-
-
-
     public static class entryCSV{
         public int year;
         public int rank;
@@ -31,42 +24,8 @@ public class Main {
         System.out.println(dataset.length); //ANSWER 1
         dataset = removeInvalidProfits(dataset).stream().toArray(String[][]::new);;
         System.out.println(dataset.length);
-        dataset = printTop20(dataset).stream().toArray(String[][]::new);
-        Arrays.stream(dataset).forEach(row -> System.out.println(   row[0]+ ", " +row[1]+ ", " +row[2]+ ", " +row[3]+ ", " +row[4]    ));
 
     }
-
-
-    public static List<String[]> printTop20 (String[][] dataset){
-        //List<String[]> ret = new ArrayList<String[]>();
-        List<String[]> queue = new ArrayList<String[]>();
-        boolean added = false;
-
-        for(String[] i : dataset){
-            float profit = Float.parseFloat(i[4]);
-            if(queue.size() > 19){
-                for (String[] j : queue){
-                    float contrast = Float.parseFloat(j[4]);
-
-                    if(profit > contrast && !added){
-                        added = true;
-                        queue.set (queue.indexOf(j), i);
-                    }
-                }
-                added = false;
-            }
-            else{
-                queue.add(i);
-            }
-        }
-
-        
-        return queue;
-    }
-
-
-
-
 
 
     //answer 2: loops thru the dataset and only adds the rows back if the matcher finds an entry that is some floating number
@@ -76,7 +35,7 @@ public class Main {
         
         for(String[] i : dataset){
             Pattern pat = Pattern.compile(pattern);
-            Matcher match = pat.matcher(i[4]);
+            Matcher match = pat.matcher(i[3]);
             if(match.find()){
                 ret.add(i);
             }
