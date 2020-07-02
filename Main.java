90f6ca1ebc16749a4d639f890c6887155070f26a
Idries Kysia <idries-kysia@uiowa.edu>
2020-07-02 16:46:38 -0500

    answer1 done

--- Main.java
+++ Main.java
@@ -2,22 +2,59 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class Main {
-
-    public static void main(String[] args) {
+    public static class entryCSV{
+        public int year;
+        public int rank;
+        public String company;
+        public int profit;
 
     }
 
-    public String[] readCSV(String path) {
+
+    public static void main(String[] args) {
+        String[][] dataset = readCSV("data.csv").stream().toArray(String[][]::new);
+        System.out.println(dataset.length); //ANSWER 1
+        dataset = removeInvalidProfits(dataset).stream().toArray(String[][]::new);;
+        System.out.println(dataset.length);
+    }
+
+    public static List<String[]> removeInvalidProfits(String[][] dataset){
+        String pattern = "^-?\\d*\\.{0,1}\\d+$";
+        List<String[]> ret = new ArrayList<String[]>();
+        
+        for(String[] i : dataset){
+            Pattern pat = Pattern.compile(pattern);
+            Matcher match = pat.matcher(i[3]);
+            if(match.find()){
+                ret.add(i);
+            }
+        }
+
+        return (String[][])ret.toArray();
+    }
+
+    public static List<String[]> readCSV(String path) {
         BufferedReader csvReader;
         String[] data = {};
+        List<String[]> dataset = new ArrayList<String[]>();
         try {
             csvReader = new BufferedReader(new FileReader(path));
             String row = "";
             try {
                 while ((row = csvReader.readLine()) != null) {
+                    
                     data = row.split(",");
+                    
+                    if(data[0] != "Year"){
+                    dataset.add(data);
+                    }
                     // do something with the data
                 }
             } catch (IOException e1) {
@@ -33,7 +70,9 @@ public class Main {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        return data;
+
+
+        return dataset;
         
     }
 
